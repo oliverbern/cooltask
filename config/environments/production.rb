eed9954a73e6cc04f1159613cabd1b8d8234fb08
oliverbern <oapjones@gmail.com>
2014-10-21 23:03:13 +0200

    fix2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+	
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'http://radiant-atoll-5684.herokuapp.com/' }
+  
 end
